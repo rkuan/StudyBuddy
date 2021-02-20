6a5a494c4e9185349b51429846635477b5ba0daa
Kayla Kelsall <kelsall@usc.edu>
2021-02-20 11:51:33 -0800

    Made studyrm pg1

--- StudyBuddy/Study Room/StudyRoom.swift
+++ StudyBuddy/Study Room/StudyRoom.swift
@@ -22,15 +22,71 @@ struct StudyRoom_Previews: PreviewProvider {
 
 struct Study: View {
     var body: some View {
-       
+        
         VStack{
             // vertical stack
-            Text("create a room")
-                .font(.system(size: 30, weight: .bold))
-                .position(x: 207, y: 50)
-            Text("select a study mode:")
-                .foregroundColor(.black)
+            Text("Create a Room")
+                .font(.custom("AvenirNextCondensed-DemiBold", size: 45))
+                .foregroundColor(Color("darkpinktext"))
+                .fontWeight(.bold)
+                .padding(.top, 75)
+                // padding --> .padding(.all, 20)
             
+            Text("select a study mode:")
+                .font(.custom("AvenirNextCondensed-Italic", size: 25))
+                .foregroundColor(.black)
+                .position(x: 207, y: 70)
+            
+            ZStack{
+            RoundedRectangle(cornerRadius: 15, style:.continuous)
+                .frame(width: 350, height: 100)
+                .foregroundColor(Color("lightpink"))
+                .padding(.bottom, 20)
+                .position(x: 207, y: -150)
+                Text("Solo Study")
+                    .font(.custom("AvenirNextCondensed-DemiBoldItalic", size: 28))
+                    .foregroundColor(Color("darkpink"))
+                    .position(x: 207, y: -155)
+                    .offset(x: 2, y: 2)
+                Text("Solo Study")
+                    .font(.custom("AvenirNextCondensed-DemiBoldItalic", size: 28))
+                    .foregroundColor(.white)
+                    .position(x: 207, y: -155)
+                
+            ZStack{
+            RoundedRectangle(cornerRadius: 15, style:.continuous)
+                .frame(width: 350, height: 100)
+                .foregroundColor(Color("lightpink"))
+                .padding(.bottom, 20)
+                .position(x: 207, y: 0)
+                Text("Study Buddy")
+                    .font(.custom("AvenirNextCondensed-DemiBoldItalic", size: 28))
+                    .foregroundColor(Color("darkpink"))
+                    .position(x: 207, y: -5)
+                    .offset(x: 2, y: 2)
+                Text("Study Buddy")
+                    .font(.custom("AvenirNextCondensed-DemiBoldItalic", size: 28))
+                    .foregroundColor(.white)
+                    .position(x: 207, y: -5)
+                
+            ZStack{
+            RoundedRectangle(cornerRadius: 15, style:.continuous)
+                .frame(width: 350, height: 100)
+                .foregroundColor(Color("lightpink"))
+                .padding(.bottom, 20)
+                .position(x: 207, y: 150)
+                Text("Class Study")
+                    .font(.custom("AvenirNextCondensed-DemiBoldItalic", size: 28))
+                    .foregroundColor(Color("darkpink"))
+                    .position(x: 207, y: 145)
+                    .offset(x:2, y:2)
+                Text("Class Study")
+                    .font(.custom("AvenirNextCondensed-DemiBoldItalic", size: 28))
+                    .foregroundColor(.white)
+                    .position(x: 207, y: 145)
         }
+        }
+}
+        } .background(Color("background"))
     }
 }
